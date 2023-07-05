9aadd810e623c09f212be12ddfce43d2c78b3c2e
ajayshastry08 <95415190+ajayshastry08@users.noreply.github.com>
2023-07-05 22:36:40 +0530

    Update coco_transformer.py

--- config/DINO/coco_transformer.py
+++ config/DINO/coco_transformer.py
@@ -1,7 +1,16 @@
+#for citypersons v1
+# data_aug_scales = [544, 576, 608, 640, 672, 704, 736, 768, 800,832, 864, 896, 928]
+# data_aug_max_size = 1856
+# data_aug_scales2_resize = [500, 600,700]
+# data_aug_scales2_crop = [428, 680]
+
+#for cityperson v2
 data_aug_scales = [480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800]
 data_aug_max_size = 1333
 data_aug_scales2_resize = [400, 500, 600]
 data_aug_scales2_crop = [384, 600]
+
+#for caltech
 # data_aug_scales = [480, 512, 544, 576, 608, 640]
 # data_aug_max_size = 640
 # data_aug_scales2_resize = [400, 500]
